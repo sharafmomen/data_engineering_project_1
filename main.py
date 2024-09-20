3ff46bd83e6e107663fbe4b9871e69395566de33
sharafmomen <sharaf.awad23@gmail.com>
2024-09-20 15:17:16 +0100

    filter applied

--- main.py
+++ main.py
@@ -8,14 +8,17 @@ from src.scraper import (
 )
 from src.transform import (
     retrieve_year_quarter, 
-    melt_and_filter_rows
+    melt_df, 
+    filter_df
 )
 
 def main():
     top_link_response = extract_from_top_link()
     excel_link = get_excel_link(top_link_response)
     filter_from, info_df = get_info(excel_link)
-    filtered_df = melt_and_filter_rows(info_df, filter_from)
+    melted_df = melt_df(info_df)
+    filtered_df = filter_df(melted_df, filter_from)
+    print(filtered_df)
 
 if __name__ == "__main__":
     main()
--- src/transform.py
+++ src/transform.py
@@ -20,16 +20,33 @@ def remove_note_data(col_val):
             return col_val[:idx]
     return col_val
 
-def melt_and_filter_rows(
-        df: pd.DataFrame, 
-        first_affect_quarter: tuple
+def filter_rows(
+        value: str,
+        filter_from: tuple, 
+    ) -> bool:
+    filter_from_str = str(filter_from[0]) + " " + str(filter_from[1])
+    return value >= filter_from_str
+
+def melt_df(
+        df: pd.DataFrame
 ):
     df.columns = [retrieve_year_quarter(col) for col in df.columns]
     df_melted = pd.melt(df, id_vars=df.columns[0], var_name="year_quarter", value_name="supply")
     df_melted = df_melted.rename(columns={df.columns[0]: "resource"})
     df_melted[["year", "quarter"]] = df_melted["year_quarter"].str.split(" ", expand=True).astype(int)
     df_melted["resource"] = df_melted["resource"].apply(remove_note_data)
-    print(df_melted.info())
-    print(df_melted.head(10))
+    return df_melted
+
+def filter_df(
+    df: pd.DataFrame, 
+    first_affect_quarter: tuple
+): 
+    """
+    filters and also removes column NO LONGER necessary: year_quarter
+    """
+    filter_condition = df["year_quarter"].apply(filter_rows, args=(first_affect_quarter,))
+    df_filtered = df[filter_condition]
+    df_filtered = df_filtered.drop("year_quarter", axis=1)
+    return df_filtered
 
 
